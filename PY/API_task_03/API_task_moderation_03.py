972ada513f016291ae07da6d91e045ab3444410c
pat-podolski-sb <patryk.podolski@smartbear.com>
2024-03-21 10:46:52 +0100

    refactor folder structure

---
+++ PY/API_task_03/API_task_moderation_03.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+from ai_devs2_utils import get_token_from_api, get_task_from_api, send_answer_to_api
+
+
+def api_test_task_03(taskName):
+  # Get TOKEN
+  tokenObject = get_token_from_api(taskName)
+  pprint('TOKEN FROM get_token_from_api METHOD:')
+  pprint(tokenObject.get('token'))
+
+
+  # Get TASK
+  taskObject = get_task_from_api(tokenObject.get('token'))
+  pprint('TASK FROM get_task_from_api METHOD:')
+  pprint(taskObject)
+
+  # # Send ANSWER
+  # answerObject = send_answer_to_api(tokenObject.get('token'), taskObject.get('cookie'))
+  # pprint('ANSWER FROM send_answer_to_api METHOD:')
+  # pprint(answerObject)
---
+++ PY/__init__.py
@@ -0,0 +1,11 @@
+from API_task_01.api_API_task_helloapi import api_test_task_01
+from API_task_02.API_task_blogger_02 import api_test_task_02
+from API_task_03.API_task_moderation_03 import api_test_task_03
+
+
+
+
+# 01 API task 
+api_test_task_01('helloapi')
+api_test_task_02('blogger')
+api_test_task_03('moderation')
