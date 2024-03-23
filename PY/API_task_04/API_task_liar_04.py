0f3ded6087838dd8446d4cd7cb5d1d1409e83bf2
pat-podolski-sb <patryk.podolski@smartbear.com>
2024-03-23 19:51:04 +0100

    solution to task liar

--- PY/API_task_04/API_task_liar_04.py
+++ PY/API_task_04/API_task_liar_04.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import json
 import os
+import requests
 from openai import OpenAI
 from ai_devs2_utils import get_token_from_api, get_task_from_api, send_answer_to_api
 
@@ -16,34 +17,46 @@ def api_test_task_04(taskName):
   pprint('TASK FROM get_task_from_api METHOD:')
   pprint(taskObject)
 
-#   {'code': 0,
-#  'hint1': "please send your question in 'question' field to /task/ endpoint "
-#           '(simple form, not JSON)',
-#  'hint2': "sometimes I don't tell the truth",
-#  'hint3': "Send to /answer/ info if I'm telling the truth. Just value: YES/NO",
-#  'msg': 'send me any question in english, and I will try to answer it in max '
-#         '150 tokens'}
-
-  # client = OpenAI(
-  #   api_key=os.getenv('OPENAI_API_KEY')
-  # )
+  client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY')
+  ) 
   
-  # arrayOfAnswers = []
+  # Create a random question about geography
+  openApiQuestionResponse = client.chat.completions.create(
+    messages=[
+      {"role": "user", "content": 'Please generate me one random question about a geography.'},
+    ],
+    model="gpt-3.5-turbo",
+    max_tokens=2200,
+    temperature=1
+  )
+  pprint('QUESTION FROM call_openai_api METHOD:')
+  randomQuestionAboutGeography = openApiQuestionResponse.choices[0].message.content
+  pprint(randomQuestionAboutGeography)
+
+  # Send POST to /task/ endpoint to get the answer for the question
+  taskPostResponse = requests.post(
+    'https://tasks.aidevs.pl/task/' + tokenObject.get('token'), data={'question': randomQuestionAboutGeography}) 
+  taskPostResponse.raise_for_status()
+  pprint('TASK POST RESPONSE:')
+  taskPostResponseAnswer = taskPostResponse.json().get('answer')
+  pprint(taskPostResponseAnswer)
   
-  # for item in taskObject.get('input'):
-  #   itemResponse = client.moderations.create(
-  #     input=item,
-  #     model='text-moderation-latest'
-  #   )
-  #   isFlagged = itemResponse.results[0].flagged
-  #   pprint('ANSWER FROM call_openai_api METHOD:')
-  #   pprint(isFlagged)
-  #   arrayOfAnswers.append(int(isFlagged))
+  # Verify the answer to the question
+  openApiAnswerVsQuestionResponse = client.chat.completions.create(
+    messages=[
+      {"role": "user", "content": 'Please tell me if the answer to the provided question is correct or not. Respond YES/NO. '+randomQuestionAboutGeography+' '+taskPostResponseAnswer}
+    ],
+    model="gpt-3.5-turbo",
+    max_tokens=2200,
+    temperature=1
+  )
+  
+  pprint('ANSWER FROM call_openai_api METHOD:')
+  isAnswerCorrectForGivenQuestion = openApiAnswerVsQuestionResponse.choices[0].message.content
+  pprint(openApiAnswerVsQuestionResponse.choices[0].message.content)
 
-  # pprint('ANSWER FROM call_openai_api METHOD:')
-  # pprint(arrayOfAnswers)
-
-  # # Send ANSWER
-  # answerObject = send_answer_to_api(tokenObject.get('token'), arrayOfAnswers)
-  # pprint('ANSWER FROM send_answer_to_api METHOD:')
-  # pprint(answerObject)
+  # Submit result to the API
+  answerObject = send_answer_to_api(tokenObject.get('token'), isAnswerCorrectForGivenQuestion)
+  pprint('ANSWER FROM send_answer_to_api METHOD:')
+  pprint(answerObject)
