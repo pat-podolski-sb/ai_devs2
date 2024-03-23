ab53af92a7e2fe7c333436e398db891d424c6f74
pat-podolski-sb <patryk.podolski@smartbear.com>
2024-03-23 20:30:36 +0100

    alternative solution for liar task

--- PY/AI_DEVs_2_tasks.py
+++ PY/AI_DEVs_2_tasks.py
@@ -2,7 +2,7 @@ from API_task_01.api_API_task_helloapi import api_test_task_01
 from API_task_02.API_task_blogger_02 import api_test_task_02
 from API_task_03.API_task_moderation_03 import api_test_task_03
 from API_task_04.API_task_liar_04 import api_test_task_04
-
+from API_task_04.API_task_liar_alternative_04 import api_test_task_alternative_04
 
 
 
@@ -10,4 +10,5 @@ from API_task_04.API_task_liar_04 import api_test_task_04
 # api_test_task_01('helloapi')
 # api_test_task_02('blogger')
 # api_test_task_03('moderation')
-api_test_task_04('liar')
+# api_test_task_04('liar')
+api_test_task_alternative_04('liar')
---
+++ PY/API_task_04/API_task_liar_alternative_04.py
@@ -0,0 +1,39 @@
+from pprint import pprint
+import requests
+import re
+from openai import OpenAI
+from ai_devs2_utils import get_token_from_api, get_task_from_api, send_answer_to_api
+
+def api_test_task_alternative_04(taskName):
+  # Get TOKEN
+  tokenObject = get_token_from_api(taskName)
+  pprint('TOKEN FROM get_token_from_api METHOD:')
+  pprint(tokenObject.get('token'))
+
+
+  # Get TASK
+  taskObject = get_task_from_api(tokenObject.get('token'))
+  pprint('TASK FROM get_task_from_api METHOD:')
+  pprint(taskObject)
+
+  # Send POST to /task/ endpoint to get the answer for the question
+  mainQuestion = 'What is the capital of Argentina?'
+  answerInLowerCase = 'buenos aires'
+  
+  taskPostResponse = requests.post(
+    'https://tasks.aidevs.pl/task/' + tokenObject.get('token'), data={'question': mainQuestion}) 
+  taskPostResponse.raise_for_status()
+  pprint('TASK POST RESPONSE:')
+  taskPostResponseAnswer = taskPostResponse.json().get('answer')
+  pprint(taskPostResponseAnswer)
+  
+  doesAnswerMatchQuestion = 'NO'
+  if re.search(answerInLowerCase, taskPostResponseAnswer.lower()):
+    doesAnswerMatchQuestion = 'YES'
+
+
+  # Submit result to the API
+  pprint('Is the answer correct for the question? => '+doesAnswerMatchQuestion)
+  answerObject = send_answer_to_api(tokenObject.get('token'), doesAnswerMatchQuestion)
+  pprint('ANSWER FROM send_answer_to_api METHOD:')
+  pprint(answerObject)
