0e90726684856687c3a0f19a3d01ef92b0ccdcbd
pat-podolski-sb <patryk.podolski@smartbear.com>
2024-03-28 09:19:50 +0100

    task 07 resolved

--- PY/API_task_07/API_task_whisper_07.py
+++ PY/API_task_07/API_task_whisper_07.py
@@ -17,20 +17,25 @@ def api_test_whisper_07(taskName):
   taskObject = get_task_from_api(tokenObject.get('token'))
   pprint('Object FROM get_task_from_api METHOD:')
   pprint(taskObject)
+
+  client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY')
+  ) 
   
-  # client = OpenAI(
-  #   api_key=os.getenv('OPENAI_API_KEY')
-  # ) 
+  # Create a random question about geography
+  mp3File = requests.get('https://tasks.aidevs.pl/data/mateusz.mp3')
+  with open('./API_task_07/mateusz.mp3', 'wb') as f:
+    f.write(mp3File.content)
+    
+  openApiQuestionResponse = client.audio.transcriptions.create(
+    file=open('./API_task_07/mateusz.mp3', 'rb'),
+    model="whisper-1",
+    response_format="text"
+  )
+  pprint('Number of Embeddings FROM call_openai_api METHOD:')
+  pprint(openApiQuestionResponse)
   
-  # # Create a random question about geography
-  # openApiQuestionResponse = client.embeddings.create(
-  #   input="Hawaiian pizza",
-  #   model="text-embedding-ada-002"
-  # )
-  # pprint('Number of Embeddings FROM call_openai_api METHOD:')
-  # pprint(len(openApiQuestionResponse.data[0].embedding))
-  
-  # # Submit result to the API
-  # answerObject = send_answer_to_api(tokenObject.get('token'), openApiQuestionResponse.data[0].embedding)
-  # pprint('ANSWER FROM send_answer_to_api METHOD:')
-  # pprint(answerObject)
+  # Submit result to the API
+  answerObject = send_answer_to_api(tokenObject.get('token'), openApiQuestionResponse)
+  pprint('ANSWER FROM send_answer_to_api METHOD:')
+  pprint(answerObject)
