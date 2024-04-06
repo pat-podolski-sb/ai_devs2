f59d16edf478744c7f889ee69a028d34e3fd2563
pat-podolski-sb <patryk.podolski@smartbear.com>
2024-04-06 22:11:14 +0200

    solved task 11

--- PY/AI_DEVs_2_tasks.py
+++ PY/AI_DEVs_2_tasks.py
@@ -32,6 +32,6 @@ from utils import calculateTokensInOpenaiInput
 # api_test_rodo_09('rodo')
 # api_test_rodo_alternative_09('rodo')
 # api_test_scraper_10('scraper')
-# api_test_whoami_11('whoami') # TODO: Implement the logic of the task
+api_test_whoami_11('whoami') # TODO: Implement the logic of the task
 # api_test_search_12('search')
-api_test_people_13('people')
+# api_test_people_13('people')
--- PY/API_task_11/API_task_whoami_11.py
+++ PY/API_task_11/API_task_whoami_11.py
@@ -12,32 +12,63 @@ def api_test_whoami_11(taskName):
   pprint('TOKEN FROM get_token_from_api METHOD:')
   pprint(tokenObject.get('token'))
 
-  # Get TASK
-  taskObject = get_task_from_api(tokenObject.get('token'))
-  pprint('Object FROM get_task_from_api METHOD:')
-  print(taskObject)
+  # # Get TASK
+  # taskObject = get_task_from_api(tokenObject.get('token'))
+  # pprint('Object FROM get_task_from_api METHOD:')
+  # print(taskObject)
 
   # TODO: Implement the logic of the task
 
-  # client = OpenAI(
-  #   api_key=os.getenv('OPENAI_API_KEY')
-  # ) 
+  client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY')
+  ) 
   
-  # openResponseWithJSONFnDefinition = client.chat.completions.create(
-  #   messages=[
-  #     {"role": "system", "content": taskObject.get('msg') + ". Article: " +  taskObject.get('input')},
-  #     {"role": "user", "content": taskObject.get('question')},
-  #   ],
-  #   model="gpt-3.5-turbo",
-  #   max_tokens=2200,
-  #   temperature=1
-  # )
-  # pprint('Function definition FROM call_openai_api METHOD:')
-  # answerToQuestionBasedOnArticle = openResponseWithJSONFnDefinition.choices[0].message.content
-  # print(answerToQuestionBasedOnArticle)
+  list_of_hints = []
+  is_model_certain = "NO"
+  
+  while is_model_certain == "NO":
+      # Get TOKEN
+    tokenObject = get_token_from_api(taskName)
+    pprint('TOKEN FROM get_token_from_api METHOD:')
+    pprint(tokenObject.get('token'))
+    
+      # Get TASK
+    taskObject = get_task_from_api(tokenObject.get('token'))
+    pprint('Object FROM get_task_from_api METHOD:')
+    print(taskObject)
+    
+    list_of_hints.append(taskObject.get('hint'))
+    openResponseWithJSONFnDefinition = client.chat.completions.create(
+      messages=[
+        {"role": "system", "content": "Are you certain about what famous person is that information? Return YES/NO." },
+        {"role": "user", "content": json.dumps(list_of_hints)},
+      ],
+      model="gpt-3.5-turbo",
+      max_tokens=2200,
+      temperature=1
+    )
+    pprint('Function definition FROM call_openai_api METHOD:')
+    answerToQuestionBasedOnArticle = openResponseWithJSONFnDefinition.choices[0].message.content
+    print(answerToQuestionBasedOnArticle)
+    is_model_certain = answerToQuestionBasedOnArticle
+  
+  openResponseWithJSONFnDefinition = client.chat.completions.create(
+    messages=[
+      {"role": "system", "content": "I'm famous person. Who am I based on given hints. Please Return only name and lastname. " },
+      {"role": "user", "content": json.dumps(list_of_hints)},
+    ],
+    model="gpt-3.5-turbo",
+    max_tokens=2200,
+    temperature=1
+  )
+  pprint('Function definition FROM call_openai_api METHOD:')
+  answerToQuestionBasedOnArticle = openResponseWithJSONFnDefinition.choices[0].message.content
+  print(answerToQuestionBasedOnArticle)
 
-  # # Submit result to the API
   
-  # answerObject = send_answer_to_api(tokenObject.get('token'), answerToQuestionBasedOnArticle)
-  # pprint('ANSWER FROM send_answer_to_api METHOD:')
-  # pprint(answerObject)
+
+  # Submit result to the API
+  tokenObject = get_token_from_api(taskName)
+  answerObject = send_answer_to_api(tokenObject.get('token'), answerToQuestionBasedOnArticle)
+  pprint('ANSWER FROM send_answer_to_api METHOD:')
+  pprint(answerObject)
